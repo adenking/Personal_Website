b4ba27c910b0dfbf765fb13198621c4d981e37a4
Aden King <adenking@live.com>
2017-09-05 02:44:54 +1000

    index now outputs meaningful data

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, this is the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
