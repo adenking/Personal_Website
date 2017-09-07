59c7a4179bb12aae92ac2cdfbe37723776a195f2
Aden King <adenking@live.com>
2017-09-07 22:14:32 +1000

    add more indepth tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,6 +9,19 @@ from .models import Question
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
+        """checks to see if future content if recent"""
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """checks to make sure content that is old isn't recent"""
+        time = timezone.now() - datetime.timedelta(days=1, seconds= 1)
+        old_question = Question(pub_date=time)
+        self.assertsIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """checks to make sure content that is recent is recent"""
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=1)
+        recent_question = Question(pub_date=time)
+        self.assertsIs(recent_question.was_published_recently(), True)
