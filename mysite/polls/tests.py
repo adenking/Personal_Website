995b2f3c7f3ddf90e08e80483cb102f8482081af
Aden King <adenking@live.com>
2017-09-08 14:48:53 +1000

    add tests for detailview

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -90,3 +90,20 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """detail view of question in the future should return 404"""
+        future_question = create_question(question_text='Future Question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_past_question(self):
+        """detail view of question in the past should return the question"""
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id))
+        response = self.client.get(url)
+        self.assertEqual(response, past_question.question_text)
