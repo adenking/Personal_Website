a820243f3a377300fae3f4b4a189553087f0fa0b
Aden King <adenking@live.com>
2017-09-10 18:08:53 +1000

    create model for blog entries

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class BlogEntry(models.Model):
+    blog_title = models.CharField(max_length=200)
+    blog_text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.blog_title
